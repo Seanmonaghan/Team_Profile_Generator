20a26735d5631349156f365cd04008d7b7f94c4a
Sean Monaghan <smonagha@conncoll.edu>
2021-03-25 19:40:12 -0400

    organized files and set up basic inquirer logic for asking about employee

---
+++ index.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser() {
+    return inquirer.prompt([{
+            type: "input",
+            name: "title",
+            message: "Who is the team Manager?"
+        }, 
+        {
+            type: "input",
+            name: "id",
+            message: "What is the Employee's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Employee's email?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is their office number?"
+        },
+        {
+            type: "list",
+            name: "continue",
+            message: "What is their office number?"
+        }
+        
+    ]);
+}
+
+function generateHTML(answers) {
+    return ``;
+}
+
+promptUser()
+    .then(function (answers) {
+        const html = generateHTML(answers);
+        return writeFileAsync("exampleREADME.md", html);
+    })
+    .then(function () {
+        console.log("Successfully wrote to HTML");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
