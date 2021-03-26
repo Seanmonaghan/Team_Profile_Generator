d6a5f465e48ede289a5866fd221934d47c07e50d
Sean Monaghan <smonagha@conncoll.edu>
2021-03-26 12:46:00 -0400

    added the classes for Employee, Manager, Intern, and Engineer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const inquirer = require('inquirer');
 const fs = require("fs");
 const util = require("util");
@@ -6,33 +7,34 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 
 function promptUser() {
-    return inquirer.prompt([{
-            type: "input",
-            name: "title",
-            message: "Who is the team Manager?"
-        }, 
-        {
-            type: "input",
-            name: "id",
-            message: "What is the Employee's ID?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the Employee's email?"
-        },
-        {
-            type: "input",
-            name: "number",
-            message: "What is their office number?"
-        },
-        {
-            type: "list",
-            name: "continue",
-            message: "What is their office number?"
-        }
+
+    // return inquirer.prompt([{
+    //         type: "input",
+    //         name: "title",
+    //         message: "Who is the team Manager?"
+    //     }, 
+    //     {
+    //         type: "input",
+    //         name: "id",
+    //         message: "What is the Employee's ID?"
+    //     },
+    //     {
+    //         type: "input",
+    //         name: "email",
+    //         message: "What is the Employee's email?"
+    //     },
+    //     {
+    //         type: "input",
+    //         name: "number",
+    //         message: "What is their office number?"
+    //     },
+    //     {
+    //         type: "list",
+    //         name: "continue",
+    //         message: "What is their office number?"
+    //     }
         
-    ]);
+    // ]);
 }
 
 function generateHTML(answers) {
@@ -41,12 +43,17 @@ function generateHTML(answers) {
 
 promptUser()
     .then(function (answers) {
-        const html = generateHTML(answers);
-        return writeFileAsync("exampleREADME.md", html);
+
     })
-    .then(function () {
-        console.log("Successfully wrote to HTML");
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+
+// promptUser()
+//     .then(function (answers) {
+//         const html = generateHTML(answers);
+//         return writeFileAsync("exampleREADME.md", html);
+//     })
+//     .then(function () {
+//         console.log("Successfully wrote to HTML");
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
