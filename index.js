039cc21ad6364409eda7e5bd3ccb904508fd4754
Sean Monaghan <smonagha@conncoll.edu>
2021-03-30 00:27:00 -0400

    Finished CSS styling for the final product

--- index.js
+++ index.js
@@ -132,6 +132,8 @@ console.log(manager)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>framework</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
     <link rel="stylesheet" href="src/styles.css">
 </head>
@@ -141,9 +143,9 @@ console.log(manager)
         <h1 class="title has-text-white">Programming Team</h1>
     </header>
 
-    <main class = 'managerContainer columns'>
+    <div class = 'managerContainer columns is-centered'>
         
-        <div class="card column profile">
+        <div class="card column is-three-quarters profile">
             <header class="card-header">
                 ${manager.getRole()}
             </header>
@@ -155,14 +157,16 @@ console.log(manager)
                 </div>
             </div>
         </div>
+    </div>
 `;
 }
 
 function generateEngineersHTML(engineer) {
     console.log(engineer)
     return `
+    <div class = "engineerColumns columns is-centered">
         ${engineer.map((engineer) => 
-        `<div class="card column profile">
+        `<div class="card column is-one-fifth profile">
         <header class="card-header">
             ${engineer.getRole()}
         </header>
@@ -175,15 +179,16 @@ function generateEngineersHTML(engineer) {
     </div>
     </div>
         `).join('')}
-       
+    </div>
 `       
 };
 
 function generateInternsHTML(interns) {
     console.log(interns)
     return `
+    <div class = "internColumns columns is-centered">
         ${interns.map((interns) => 
-        `<div class="card column profile">
+        `<div class="card column is-one-fifth profile">
         <header class="card-header">
             ${interns.getRole()}
         </header>
@@ -196,7 +201,7 @@ function generateInternsHTML(interns) {
     </div>
     </div>
         `).join('')}
-       
+    </div>
 `       
 
 };
